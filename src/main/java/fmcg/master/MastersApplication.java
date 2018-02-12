08f54f65c92b3f14e69f205f209bc74a4f2cf721
babucs <m.babufastmail@gmail.com>
2018-02-12 11:20:16 +0530

    Initial commit

---
+++ src/main/java/fmcg/master/MastersApplication.java
@@ -0,0 +1,12 @@
+package fmcg.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastersApplication.class, args);
+	}
+}
---
+++ src/test/java/fmcg/master/MastersApplicationTests.java
@@ -0,0 +1,16 @@
+package fmcg.master;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MastersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
